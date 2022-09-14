7e4767dc84b8ac36383b4c0e0651fc2023f4d6be
XHUY7890 <111957800+XHUY7890@users.noreply.github.com>
2022-09-14 15:02:28 +0700

    Add files via upload

---
+++ SUMDIG.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demtu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            long[] mang= new long[100002];
+            int t1 = t;
+            while (t > 0) 
+            {
+                mang[t] = Convert.ToInt64(Console.ReadLine());
+                t--;
+            }
+            while (t1 > 0)
+            {
+                Console.WriteLine("{0}", sumdig(mang[t1]));
+                t1--;
+            }
+            Console.ReadLine();
+           
+        }
+        static long  sumdig(long x)
+        {
+            long sum = 0;
+            while (x > 0)
+            {
+                sum = sum + x % 10;
+                x = x / 10;
+            }
+            return sum;
+        }
+    }
+}
